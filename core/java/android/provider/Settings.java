8082553acabe09ad351950def8d76c2f8ed915ba
doixanh <doixanh@gmail.com>
2011-08-03 16:23:28 +0200

    back button ends call
    Change-Id: I2fbecda3b1c8c6dc8d45cbd3e22a7c66e7552a99

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1154,6 +1154,13 @@ public final class Settings {
          */
         public static final String FLIPPING_DOWN_SNOOZES_ALARM = "flipping_down_snoozes_alarm";
 
+        /**
+         * Whether back button ends call or not
+         *
+         * @hide
+         */
+        public static final String BACK_BUTTON_ENDS_CALL = "back_button_ends_call";
+
 
         /**
          * Whether we keep the device on while the device is plugged in.
