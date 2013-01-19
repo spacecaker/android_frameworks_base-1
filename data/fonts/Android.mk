39796b92f2598645b0f8360399222b354002a45d
Jochen B <jochen.blacha@gmail.com>
2013-01-19 21:31:06 +0100

    Update data/fonts/Android.mk
    Jelly Bean font listing

--- data/fonts/Android.mk
+++ data/fonts/Android.mk
@@ -16,34 +16,48 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 copy_from :=                \
-    DroidSansFallback.ttf \
-    Roboto-Regular.ttf \
-    Roboto-Bold.ttf \
-    Roboto-Italic.ttf \
-    Roboto-BoldItalic.ttf \
-    Roboto-Light.ttf \
-    Roboto-LightItalic.ttf \
-    Roboto-Thin.ttf \
-    Roboto-ThinItalic.ttf \
-    RobotoCondensed-Regular.ttf \
-    RobotoCondensed-Bold.ttf \
-    RobotoCondensed-Italic.ttf \
-    RobotoCondensed-BoldItalic.ttf \
-    DroidNaskh-Regular.ttf \
-    DroidNaskh-Regular-SystemUI.ttf \
-    DroidSansDevanagari-Regular.ttf \
-    DroidSansHebrew-Regular.ttf \
-    DroidSansHebrew-Bold.ttf \
-    DroidSansThai.ttf \
-    DroidSerif-Regular.ttf \
-    DroidSerif-Italic.ttf \
-    DroidSerif-BoldItalic.ttf \
-    DroidSansMono.ttf \
-    DroidSansArmenian.ttf \
-    DroidSansGeorgian.ttf \
     AndroidClock.ttf \
     AndroidClock_Highlight.ttf \
     AndroidClock_Solid.ttf \
+    DroidKufi-Bold.ttf \
+    DroidKufi-Regular.ttf \
+    DroidNaskh-Bold.ttf \
+    DroidNaskh-Regular-SystemUI.ttf \
+    DroidNaskh-Regular.ttf \
+    DroidSansArabic.ttf \
+    DroidSansArmenian.ttf \
+    DroidSansDevanagari-Regular.ttf \
+    DroidSansEthiopic-Bold.ttf \
+    DroidSansEthiopic-Regular.ttf \
+    DroidSansFallback.ttf \
+    DroidSansFallbackFull.ttf \
+    DroidSansFallbackLegacy.ttf \
+    DroidSansGeorgian.ttf \
+    DroidSansHebrew-Bold.ttf \
+    DroidSansHebrew-Regular.ttf \
+    DroidSansJapanese.ttf \
+    DroidSansMono.ttf \
+    DroidSansTamil-Bold.ttf \
+    DroidSansTamil-Regular.ttf \
+    DroidSansThai.ttf \
+    DroidSerif-Bold.ttf \
+    DroidSerif-BoldItalic.ttf \
+    DroidSerif-Italic.ttf \
+    DroidSerif-Regular.ttf \
+    MTLc3m.ttf \
+    MTLmr3m.ttf \
+    Roboto-Bold.ttf \
+    Roboto-BoldItalic.ttf \
+    Roboto-Italic.ttf \
+    Roboto-Light.ttf \
+    Roboto-LightItalic.ttf \
+    Roboto-Regular.ttf \
+    Roboto-Thin.ttf \
+    Roboto-ThinItalic.ttf \
+    RobotoCondensed-Bold.ttf \
+    RobotoCondensed-BoldItalic.ttf \
+    RobotoCondensed-Italic.ttf \
+    RobotoCondensed-Regular.ttf \
 
 ifneq ($(NO_FALLBACK_FONT),true)
 ifeq ($(filter %system/fonts/DroidSansFallback.ttf,$(PRODUCT_COPY_FILES)),)
